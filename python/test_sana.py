b7f3ea4cf0b400bb485f90264eac54abca61ff4f
mark Pupilli <65598182+markp-gc@users.noreply.github.com>
2025-04-09 15:02:01 +0000

    Server that streams images generated from the Sana-sprint model.

---
+++ python/test_sana.py
@@ -0,0 +1,36 @@
+from diffusers import SanaPAGPipeline, SanaSprintPipeline
+import torch
+import argparse
+import time  # Add this import for timing
+import gui_server
+import numpy as np
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Generate an image using Sana model with a text prompt')
+parser.add_argument('--port', type=int, default=5000, help='Port to run the server on (default: 5000)')
+args = parser.parse_args()
+
+pipeline = SanaSprintPipeline.from_pretrained(
+    "Efficient-Large-Model/Sana_Sprint_0.6B_1024px_diffusers",
+    torch_dtype=torch.bfloat16
+)
+pipeline.to("cuda:0")
+
+server = gui_server.InterfaceServer(args.port)
+server.start()
+gui_server.set_log_level("off")
+
+test_image = np.zeros((1024, 1024, 3), dtype=np.uint8)
+server.initialise_video_stream(test_image.shape[1], test_image.shape[0])
+
+prompt="cat"
+
+while not server.get_state().stop:
+        image = pipeline(prompt=prompt, num_inference_steps=2).images[0]
+        server.send_image(np.array(image), convert_to_bgr=True)
+
+        if server.state_changed():
+            state = server.consume_state()
+            print(f"Value: {state.value}")
+
+server.stop()
